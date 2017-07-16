c06da98e3888d80aa269b62597dbbbc6844e68b0
CorpArmstrong <cherry.cake128@gmail.com>
2017-07-16 12:45:20 +0300

    Added necessary files.

---
+++ ConAnimSys/Src/CASConEventAnimation.cpp
@@ -0,0 +1,12 @@
+// =========================================================================== 
+// Project: ConAnimSys 
+// 
+// Description: 
+// This is a test file.  
+// ===========================================================================
+
+#include "ConAnimSysPrivate.h"
+
+IMPLEMENT_PACKAGE(ConAnimSys);
+
+IMPLEMENT_CLASS(ACASConEventAnimation);

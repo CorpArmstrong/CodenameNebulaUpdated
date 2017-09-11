8d8b236752227e399c73c06b4dd6b49bf281acba
CorpArmstrong <cherry.cake128@gmail.com>
2017-09-11 11:18:22 +0300

    Improved package.bat

--- CodenameNebula/package.bat
+++ CodenameNebula/package.bat
@@ -1,10 +1,11 @@
-:: Performing Cleanup
+:: Perform Cleanup
 del /q Maps\*.*
+del /q Music\*.*
 del /q Music\Ogg\*.*
 del /q Textures\*.*
 del /q System\*.*
 
-:: Packing Maps
+:: Package Maps
 copy ..\Maps\Entry.dx											Maps\
 copy ..\Maps\CNNentry.dx										Maps\
 copy ..\Maps\05_MoonIntro.dx									Maps\
@@ -18,17 +19,25 @@ echo . > dummy.txt
 cd ..
 
 :: Package Music
+copy ..\Music\Terrified.umx										Music\
 copy ..\Music\Ogg\f_demo_with_bass.ogg							Music\Ogg\
 copy ..\Music\Ogg\Mysterious_SciFi_Music_THE_LAST_FRONTIER.ogg	Music\Ogg\
 copy ..\Music\Ogg\win_xp_is_shit.ogg							Music\Ogg\
 copy "..\Music\Ogg\Solar_Smoke_Post_Silence _07_The_Spark.ogg"	Music\Ogg\
-:: Add songs from ApocalypseInside here.
+copy ..\Music\Ogg\Anthem.ogg									Music\Ogg\
+copy ..\Music\Ogg\Area51_Leaving.ogg							Music\Ogg\
+copy ..\Music\Ogg\WhoAmI.ogg									Music\Ogg\
+
+cd Music\
+echo . > dummy.txt
+cd ..
 
 cd Music\Ogg\
 echo . > dummy.txt
 cd ..\..
 
 :: Package Textures
+copy ..\Textures\Ophelia.utx									Textures\
 copy ..\Textures\AiInfoPortraits.utx							Textures\
 copy ..\Textures\AITex.utx										Textures\
 copy ..\Textures\PFADTex.utx									Textures\
@@ -37,7 +46,7 @@ copy ..\Textures\X3.utx											Textures\
 copy ..\Textures\X3tex.utx										Textures\
 copy ..\Textures\GenFX.utx										Textures\
 copy ..\Textures\CNNTextures.utx								Textures\
-::copy ..\Textures\AIStalk.utx									Textures\
+copy ..\Textures\AIStalk.utx									Textures\
 
 cd Textures\
 echo . > dummy.txt
@@ -60,6 +69,7 @@ copy ..\System\CNNUser.ini										System\
 copy ..\System\CNNStart.bat										System\
 copy ..\System\PFAD.u											System\
 copy ..\System\GaussGun.u										System\
+copy ..\System\DXRVNewVehicles.u								System\
 
 cd System\
 echo . > dummy.txt

26821beeebde48859a54dec26e32e7d18b66da28
CorpArmstrong <cherry.cake128@gmail.com>
2017-09-19 09:33:46 +0300

    Added CNNInstallUtil project.

---
+++ CNNInstallUtil/CNNInstallUtil/InstallUtil.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace CNNInstallUtil
+{
+    class InstallUtil
+    {
+        private string currentPath = Directory.GetCurrentDirectory();
+
+        public void Install()
+        {
+            string pathToModSystem = Path.Combine(currentPath, "System");
+            string pathToSystem = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).ToString(), "System");
+
+            DirectoryInfo modSystemInfo = new DirectoryInfo(pathToModSystem);
+            DirectoryInfo originalSystemInfo = new DirectoryInfo(pathToSystem);
+
+            FileInfo originalDeusExIni = new FileInfo(Path.Combine(pathToSystem, "DeusEx.ini"));
+            FileInfo originalDeusExUserIni = new FileInfo(Path.Combine(pathToSystem, "User.ini"));
+
+            if (!modSystemInfo.Exists)
+            {
+                Console.WriteLine("Error: directory {0} doesn't exists!", pathToModSystem);
+                Console.WriteLine("Creating directory!");
+                Directory.CreateDirectory(pathToModSystem); // unauthorized exception?
+            }
+
+            if (originalSystemInfo.Exists)
+            {
+                if (originalDeusExIni.Exists)
+                {
+                    File.Copy(originalDeusExIni.FullName, Path.Combine(pathToModSystem, "CNN.ini"), true);
+                    InjectIniProperties(Path.Combine(pathToModSystem, "CNN.ini"));
+                    Console.WriteLine("CNN.ini file created!");
+                }
+                else
+                {
+                    Console.WriteLine("Error: file {0} doesn't exists!", originalDeusExIni.FullName);
+                    Console.WriteLine("Creating template CNN.ini file!");
+                    CreateTemplateCNNIniFile(pathToModSystem);
+                }
+
+                if (originalDeusExUserIni.Exists)
+                {
+                    File.Copy(originalDeusExUserIni.FullName, Path.Combine(pathToModSystem, "CNNUser.ini"), true);
+                    Console.WriteLine("CNNUser.ini file created!");
+                }
+                else
+                {
+                    Console.WriteLine("Error: file {0} doesn't exists!", originalDeusExUserIni.FullName);
+                    Console.WriteLine("Skipping copy file.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error: directory {0} doesn't exists!", pathToSystem);
+            }
+        }
+
+        private void CreateTemplateCNNIniFile(string pathToModSystem)
+        {
+            StreamWriter streamWriter = new StreamWriter(Path.Combine(pathToModSystem, "CNN.ini"));
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.Append("[URL]");
+            stringBuilder.AppendLine();
+            stringBuilder.Append("Class=CNN.TantalusDenton");
+            stringBuilder.AppendLine().AppendLine();
+            stringBuilder.Append("[Engine.Engine]");
+            stringBuilder.AppendLine();
+            stringBuilder.Append("DefaultGame=CNN.CNNGameInfo");
+            stringBuilder.AppendLine().AppendLine();
+            stringBuilder.Append("[Core.System]");
+            stringBuilder.AppendLine();
+            stringBuilder.Append("SavePath=" + Path.Combine(currentPath, "Save"));
+            stringBuilder.AppendLine();
+            stringBuilder.Append("Paths=" + Path.Combine(currentPath, "Maps\\*.dx"));
+            stringBuilder.AppendLine();
+            stringBuilder.Append("Paths=" + Path.Combine(currentPath, "System\\*.u"));
+            stringBuilder.AppendLine();
+            stringBuilder.Append("Paths=" + Path.Combine(currentPath, "Textures\\*.utx"));
+            stringBuilder.AppendLine();
+            stringBuilder.Append("Paths=" + Path.Combine(currentPath, "Music\\*.umx"));
+            stringBuilder.AppendLine();
+
+            streamWriter.Write(stringBuilder.ToString());
+            streamWriter.Flush();
+            streamWriter.Close();
+        }
+
+        private void InjectIniProperties(string pathToFile)
+        {
+            List<string> iniKeyValues = new List<string>(File.ReadAllLines(pathToFile));
+
+            int idx1 = iniKeyValues.FindIndex(x => x.StartsWith("Class="));
+            iniKeyValues[idx1] = "Class=CNN.TantalusDenton";
+
+            int idx2 = iniKeyValues.FindIndex(x => x.StartsWith("DefaultGame="));
+            iniKeyValues[idx2] = "DefaultGame=CNN.CNNGameInfo";
+
+            int propertiesIdx = iniKeyValues.FindIndex(x => x.StartsWith("[Core.System]"));
+
+            iniKeyValues.Insert(++propertiesIdx, "SavePath=" + Path.Combine(currentPath, "Save"));
+            iniKeyValues.Insert(++propertiesIdx, "Paths=" + Path.Combine(currentPath, "Maps\\*.dx"));
+            iniKeyValues.Insert(++propertiesIdx, "Paths=" + Path.Combine(currentPath, "System\\*.u"));
+            iniKeyValues.Insert(++propertiesIdx, "Paths=" + Path.Combine(currentPath, "Textures\\*.utx"));
+            iniKeyValues.Insert(++propertiesIdx, "Paths=" + Path.Combine(currentPath, "Music\\*.umx"));
+
+            File.WriteAllLines(pathToFile, iniKeyValues.ToArray());
+        }
+    }
+}
---
+++ CNNInstallUtil/CNNInstallUtil/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CNNInstallUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InstallUtil installUtil = new InstallUtil();
+            installUtil.Install();
+        } 
+    }
+}
